69b70bc95b6644e789efdd73c48f08c57d5d8533
Yoshimi Kondo <yoshimi.keisui@gmail.com>
2015-11-19 16:33:00 +0900

    Initial Commit

---
+++ SimpleGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleGraph
+//
+//  Created by Yoshimi Kondo on 2015/11/19.
+//  Copyright © 2015年 yoshimikeisui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
